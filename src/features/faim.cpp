361aa8749b51f45dbdad65e7765ee68b470d6ca7
Justin Kinnaird <jkinnaird1991@gmail.com>
2018-12-06 20:33:23 -0600

    Fix recoil

--- src/features/faim.cpp
+++ src/features/faim.cpp
@@ -46,7 +46,7 @@ void FAim::Recoil(uintptr_t localPlayer, bool forceReset)
     Vector punchAngle;
     Vector viewAngle;
     int shotsFired = 0;
-    m_mem.Read(localPlayer + 0xabd0, &shotsFired);
+    m_mem.Read(localPlayer + 0xac90, &shotsFired);
     if (shotsFired > 1) {
         m_mem.Read(Offset::Engine::ClientState + Offset::Static::ViewAngles, &viewAngle);
         if (m_mem.Read(localPlayer + Netvar::CBasePlayer::Local::m_aimPunchAngle, &punchAngle)) {
